3b7e80976fb78b18c475de1b0a54ca581aba786f
Abdelrahman Mostafa <55556764+Abdelrhmxn@users.noreply.github.com>
2023-09-22 16:24:52 +0300

    Update installnginx.groovy

--- vars/installnginx.groovy
+++ vars/installnginx.groovy
@@ -8,6 +8,16 @@ def call(){
                 sh "sudo apt install nginx"
             }
         }
+        stage('start nginx') {
+            steps {
+                sh "sudo systemctl start nginx"
+            }
+        }
+        stage('test nginx') {
+            steps {
+                sh "curl localhost"
+            }
+        }
     }
 } 
 }
